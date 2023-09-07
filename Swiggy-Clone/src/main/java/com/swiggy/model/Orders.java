9b2ef2a5761029f4f225043d1c0a5a0397cc4652
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-07 13:45:21 +0530

    Orders entity created inside model package

---
+++ Swiggy-Clone/src/main/java/com/swiggy/model/Orders.java
@@ -0,0 +1,54 @@
+package com.swiggy.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Orders {// owning side
+//	Order: a unique orderId, customerId, restaurantId, deliveryPartnerId (nullable), items (a list of menu items), and orderStatus.
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer orderId;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Customer customer;
+	
+//	@ManyToOne(cascade = CascadeType.ALL)
+//	private Restaurant restaurant;
+		
+	@ManyToOne(cascade = CascadeType.ALL)
+	private DeliveryPartner deliveryPartner;
+	
+	@ElementCollection
+	private List<String> items;
+	
+	@Enumerated
+	private OrderStatus orderStatus;
+
+	public Orders(@NotNull(message = "Provide order Id") Customer customer,
+			 DeliveryPartner deliveryPartner,
+			List<String> items, OrderStatus orderStatus) {
+		super();
+		this.customer = customer;
+		this.deliveryPartner = deliveryPartner;
+		this.items = items;
+		this.orderStatus = orderStatus;
+	}
+}
