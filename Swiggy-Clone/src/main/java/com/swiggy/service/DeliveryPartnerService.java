edaeea5ccfc815d6ba2a20f452e01c206a83859f
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-09 13:47:17 +0530

    Abstract_method_of_DeliveryPartner_are_created

---
+++ Swiggy-Clone/src/main/java/com/swiggy/service/DeliveryPartnerService.java
@@ -0,0 +1,15 @@
+package com.swiggy.service;
+
+import java.util.List;
+
+import com.swiggy.model.DeliveryPartner;
+
+import jakarta.validation.Valid;
+
+public interface DeliveryPartnerService {
+	
+	public DeliveryPartner addDeliveryPartner(@Valid DeliveryPartner deliveryPartner);
+	public List<DeliveryPartner> getAllDeliveryPartner();
+	public List<DeliveryPartner> getDeliveryPartnerPageWise(Integer pageNumber, Integer recordsPerPage);
+	public List<DeliveryPartner> getDeliveryPartnerBySorting(String field, String direction);
+}
