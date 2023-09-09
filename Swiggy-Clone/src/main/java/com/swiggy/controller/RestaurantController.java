2f1551c39abc283f3f13324c3770ff0f0a41c2e7
Deependra Yadav <deependrayadav6306@gmail.com>
2023-09-09 15:44:23 +0530

    returant_and_deliveryPartner_controller_implemnetation_done

---
+++ Swiggy-Clone/src/main/java/com/swiggy/controller/RestaurantController.java
@@ -0,0 +1,45 @@
+package com.swiggy.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swiggy.model.Restaurant;
+import com.swiggy.service.RestaurantService;
+
+import jakarta.validation.Valid;
+@RestController
+@CrossOrigin("*")
+public class RestaurantController {
+
+	@Autowired
+	private RestaurantService rs;
+	
+	@PostMapping(value = "/resturants")
+	public ResponseEntity<Restaurant> addResturant(@Valid @RequestBody Restaurant res){
+		return new ResponseEntity<Restaurant>(rs.addResturant(res),HttpStatus.CREATED);
+	}
+	
+	@GetMapping(value = "/resturants")
+	public ResponseEntity<List<Restaurant>> getRestaurant(){
+		return new ResponseEntity<List<Restaurant>>(rs.getRestaurant(),HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/resturants_by_page/{pageNumber}/{recordsperPage}")
+	public ResponseEntity<List<Restaurant>> getRestaurantPageWise(@PathVariable Integer pageNumber,@PathVariable Integer recordsperPage){
+		return new ResponseEntity<List<Restaurant>>(rs.getRestaurantPageWise(pageNumber,recordsperPage),HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/resturants_by_sorting/{field}/{direction}")
+	public ResponseEntity<List<Restaurant>> getRestaurantBySorting(@PathVariable String field,@PathVariable String direction){
+		return new ResponseEntity<List<Restaurant>>(rs.getRestaurantBySorting(field,direction),HttpStatus.OK);
+	}
+}
